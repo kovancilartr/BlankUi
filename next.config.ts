3ee8274c4052c36a9e26f5b6a1a8e56902747f6d
Yalçın <kovancilartr@gmail.com>
2024-11-04 22:17:31 +0300

    first

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["mekovancilar.pockethost.io","images.pexels.com"], // Dış alan adını buraya ekleyin
+  },
 };
 
 export default nextConfig;
